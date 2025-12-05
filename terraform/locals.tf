94727e22bfc04236f20c51dc19810fedb98880a1
Brandon <brandon@parksbra.com>
2025-12-04 20:01:57 -0800

    update var ref

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -7,7 +7,7 @@ locals {
   service_network_cidr = data.kubernetes_config_map.network_context.data.service_network_cidr
   cluster_domain = data.kubernetes_config_map.network_context.data.cluster_domain
 
-  cluster_issuer_enabled = tobool(data.kubernetes_config_map.network_context.data.cert_manager_cluster_issuer_enabled)
+  cluster_issuer_created = tobool(data.kubernetes_config_map.network_context.data.cert_manager_cluster_issuer_created)
   cluster_issuer_name = data.kubernetes_config_map.network_context.data.cert_manager_cluster_issuer_name
 
   dns_records_proxy_enabled = tobool(data.kubernetes_config_map.network_context.data.dns_records_proxy_enabled)
@@ -26,7 +26,7 @@ locals {
   kubernetes_dashboard_ingress_class_name = local.ingress_class_name
   kubernetes_dashboard_ingress_host_address = "dashboard.k8s.${local.external_domain}"
   kubernetes_dashboard_chart_linting_enabled = true
-  kubernetes_dashboard_ingress_annotations = local.cluster_issuer_enabled ? {
+  kubernetes_dashboard_ingress_annotations = local.cluster_issuer_created ? {
     "cert-manager.io/cluster-issuer" = local.cluster_issuer_name
   } : {}
 
