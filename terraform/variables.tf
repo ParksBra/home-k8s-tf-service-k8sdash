f8db03afdbc70b2ad3572ba6a3ae8d8cbe632808
Brandon <brandon@parksbra.com>
2025-12-08 16:29:30 -0800

    remove unused vars

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "azuredevops_library_project_id" {
-  description = "The Azure DevOps project ID containing the variable library"
-  type        = string
-}
-
-variable "azuredevops_library_name" {
-  description = "The name of the Azure DevOps variable library"
-  type        = string
-}
